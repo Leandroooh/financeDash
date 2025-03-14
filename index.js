96585a47bdca9a5120e725694acbd939274c2df1
Leandro <LeandroSilvaDeOliveira0605@gmail.com>
2025-03-14 14:20:15 -0300

    Front-End Update

---
+++ index.js
@@ -0,0 +1,7 @@
+import MicroModal from "https://cdn.jsdelivr.net/npm/micromodal/dist/micromodal.es.min.js";
+
+const incomebtn = document.getElementById("income-btn");
+
+MicroModal.init({
+	disableScroll: true, // Impede o scroll no fundo quando o modal está aberto
+});
